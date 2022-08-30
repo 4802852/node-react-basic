ea6a3e0e4196469a3d6ed35a52150fea852e79c6
4802852 <4802852@gmail.com>
2022-08-31 00:01:30 +0900

    react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return <div>LandingPage 안녕하세요 제발나와라요</div>;
 }
 
 export default LandingPage;
