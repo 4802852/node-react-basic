6ba854d8d33b5cabbde4f930d81519b9815c9600
4802852 <4802852@gmail.com>
2022-08-31 23:57:29 +0900

    axios, concurrently, antd framework, redux

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,12 @@
 import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 function LandingPage() {
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => console.log(response.data));
+  }, []);
+
   return <div>LandingPage 안녕하세요 제발나와라요</div>;
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "antd/dist/antd.css";
+
+import { Provider } from "react-redux";
+import { applyMiddleware, configureStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import reduxThunk from "redux-thunk";
+import { Reducer } from "./_reducers";
+
+const configureStoreWithMiddleware = applyMiddleware(promiseMiddleware, reduxThunk)(configureStore);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
